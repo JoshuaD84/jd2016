0624289f3c5dc25661e83169c78fbf1d22660871
Joshua Hartwell <JHartwell@gmail.com>
2016-12-11 15:32:56 -0600

    Update footer email.

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
    </div> <!-- End content -->
    <div id="footer" class="constrained-to-mid">
       <a href="http://www.joshuad.net">&copy; Joshua Hartwell</a>
-      <a href="mailto:Joshua@JesterBlocks.net">Contact me</a>
+      <a href="mailto:joshua@joshuad.net">Contact me</a>
       <a href="http://www.joshuad.net/sitemap.xml">Sitemap</a> 
    </div> <!-- End Footer -->
    <? wp_footer(); ?>
